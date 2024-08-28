e8f32213acd3a82b92724f3138951bcfe78fcc9b
joshuaongg21 <joshuaongg21@gmail.com>
2024-08-28 09:27:47 +0000

    slight changes in bash script

--- evaluate.sh
+++ evaluate.sh
@@ -2,7 +2,7 @@
 
 datasets=("mmlu" "aqua" "mmlu-pro" "GPQA" "gaokao")
 methods=("non-cot" "cot" "symbolicot")
-model="llama3.1_8b"
+model="qwen2"
 dataconfig=("normal" "shuffle" "swapping")
 
 # Set the visible CUDA devices to GPU 0 and GPU 1
@@ -11,6 +11,6 @@ export CUDA_VISIBLE_DEVICES=0,1
 for dataset in "${datasets[@]}"; do
     for method in "${methods[@]}"; do
         echo "Running: python main.py --dataset $dataset --method $method --model $model --dataconfig $dataconfig"
-        python main.py --dataset "$dataset" --method "$method" --model "$model"
+        python main.py --dataset "$dataset" --method "$method" --model "$model" --dataconfig "$dataconfig"
     done
 done
