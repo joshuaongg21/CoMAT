86ec16130fed94ed9c1b0b3fff3c838a32a611ed
joshuaongg21 <joshuaongg21@gmail.com>
2024-08-28 14:02:36 +0000

    modified bash script for evaluation

--- evaluate.sh
+++ evaluate.sh
@@ -1,16 +1,20 @@
 #!/bin/bash
 
-datasets=("mmlu" "aqua" "mmlu-pro" "GPQA" "gaokao")
+datasets=("mmlu" "aqua" "GPQA" "gaokao" "mmlu-pro" "gsm8k" "mgsm")
 methods=("non-cot" "cot" "symbolicot")
-model="qwen2"
-dataconfig=("normal" "shuffle" "swapping")
+models=("codestral" "llama3.1_8b")
+dataconfigs=("normal" "shuffle" "swapping")
 
-# Set the visible CUDA devices to GPU 0 and GPU 1
+# Set the visible CUDA devices 
 export CUDA_VISIBLE_DEVICES=0,1
 
 for dataset in "${datasets[@]}"; do
     for method in "${methods[@]}"; do
-        echo "Running: python main.py --dataset $dataset --method $method --model $model --dataconfig $dataconfig"
-        python main.py --dataset "$dataset" --method "$method" --model "$model" --dataconfig "$dataconfig"
+        for model in "${models[@]}"; do
+            for dataconfig in "${dataconfigs[@]}"; do
+                echo "Running: python main.py --dataset $dataset --method $method --model $model --dataconfig $dataconfig"
+                python main.py --dataset "$dataset" --method "$method" --model "$model" --dataconfig "$dataconfig"
+            done
+        done
     done
 done
